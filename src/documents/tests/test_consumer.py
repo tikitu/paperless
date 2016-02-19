986060f181224692563cf8d5aabb7654495b88a0
Tikitu de Jager <tikitu@minddistrict.com>
2016-02-20 00:06:37 +0200

    Template-based tests of combinations of valid elements

--- src/documents/tests/test_consumer.py
+++ src/documents/tests/test_consumer.py
@@ -85,3 +85,57 @@ class TestAttachment(TestCase):
             "Τιτλε",
             self.TAGS
         )
+class Permutations(TestCase):
+    CONSUMER = Consumer()
+
+    valid_senders = ['timmy', 'Dr. McWheelie', 'Dash Gor-don', 'ο Θερμαστής']
+    valid_titles = ['title', 'Title w Spaces', 'Title a-dash', 'Τίτλος', '']
+    valid_tags = ['tag', 'tig,tag', '-', '0,1,2', '']
+    valid_suffixes = ['pdf', 'png', 'jpg', 'jpeg', 'gif']
+
+    def _test_guessed_attributes(
+            self, filename, title, suffix, sender=None, tags=None):
+        got_sender, got_title, got_tags, got_suffix = \
+            self.CONSUMER._guess_attributes_from_name(filename)
+
+        # Required
+        self.assertEqual(got_title, title, filename)
+        self.assertEqual(got_suffix, suffix, filename)
+        # Optional
+        if sender is None:
+            self.assertEqual(got_sender, sender, filename)
+        else:
+            self.assertEqual(got_sender.name, sender, filename)
+        if tags is None:
+            self.assertEqual(got_tags, (), filename)
+        else:
+            self.assertEqual([t.slug for t in got_tags], tags.split(','),
+                             filename)
+
+    def test_just_title(self):
+        template = '/path/to/{title}.{suffix}'
+        for title in self.valid_titles:
+            for suffix in self.valid_suffixes:
+                spec = dict(title=title, suffix=suffix)
+                filename = template.format(**spec)
+                self._test_guessed_attributes(filename, **spec)
+
+    def test_title_and_sender(self):
+        template = '/path/to/{sender} - {title}.{suffix}'
+        for sender in self.valid_senders:
+            for title in self.valid_titles:
+                for suffix in self.valid_suffixes:
+                    spec = dict(sender=sender, title=title, suffix=suffix)
+                    filename = template.format(**spec)
+                    self._test_guessed_attributes(filename, **spec)
+
+    def test_title_and_sender_and_tags(self):
+        template = '/path/to/{sender} - {title} - {tags}.{suffix}'
+        for sender in self.valid_senders:
+            for title in self.valid_titles:
+                for tags in self.valid_tags:
+                    for suffix in self.valid_suffixes:
+                        spec = dict(sender=sender, title=title,
+                                    tags=tags, suffix=suffix)
+                        filename = template.format(**spec)
+                        self._test_guessed_attributes(filename, **spec)
